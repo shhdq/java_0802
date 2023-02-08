bee9662b7929c93c1fe83abae578ed191d3ade2f
shhdq <54862478+shhdq@users.noreply.github.com>
2023-02-08 21:50:27 +0200

    Test fly

--- src/Main.java
+++ src/Main.java
@@ -34,14 +34,48 @@ public class Main {
 
         Main main = new Main();
 
-        main.create("Pabarot kaķi");
-        main.create("Skriet 10km");
+        Scanner scan = new Scanner(System.in);
 
-        main.update(0, "Pabarot suni");
+        System.out.println("Ko Tu vēlies darīt?: (C = create, R = read, U = update, D = delete)");
 
-        main.delete(1);
+        String usr_choice = scan.nextLine();
 
-        main.read();
+
+
+        switch (usr_choice) {
+            case "C":
+                System.out.print("Ievdi todo: ");
+                String usr_todo = scan.nextLine();
+                main.create(usr_todo);
+
+                System.out.println("Tika izveidot jauns TODO!");
+                break;
+            case "R":
+                main.read();
+                break;
+            case "U":
+                System.out.print("Kuru todo vēlies mainīt?: ");
+                int todo_index = scan.nextInt();
+
+                System.out.print("Ievadi jauno todo: ");
+                String todo_new = scan.nextLine();
+
+                main.update(todo_index, todo_new);
+
+                System.out.println("TODO tika izmainīts");
+
+                break;
+
+            case "D":
+                System.out.print("Kuru TODO vēlies dzēst? ");
+                int which_one = scan.nextInt();
+
+                main.delete(which_one);
+
+                System.out.println("TODO tika izdzēsts!");
+
+                break;
+        }
 
 
     }
